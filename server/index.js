d00df93b7300aa728ca8a99248b952fe1a93ac98
imparth7 <imparthdudhatra@gmail.com>
2024-02-27 18:12:12 +0530

    SERVER - index.js

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(ProductRouter);
 
 // Root path
 app.get("/", (req, res) => {
-  res.write("<a href=`/king`>King</a>")
+  res.write("<a href=`/king`>King URL</a>")
   res.send("Hello World!");
 })
 
